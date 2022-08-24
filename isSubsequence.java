9bd7adb1f9b7236fc8a383e492ab6b4526e684ce
PranavFokmare-Dev <80598054+PranavFokmare-Dev@users.noreply.github.com>
2022-08-24 20:08:13 +0530

    Done

--- isSubsequence.java
+++ isSubsequence.java
@@ -14,3 +14,26 @@ class Solution {
         
     }
 }
+
+//Recursive approace
+
+/*
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        return isSubsequence(s,t,0,0);
+    }
+    private boolean isSubsequence(String s, String t, int i, int j){
+
+        if(s.length() - i > t.length() - j)
+            return false;
+        if(i == s.length())return true;
+        
+        if(s.charAt(i) == t.charAt(j)){
+            return isSubsequence(s,t,i+1,j+1);
+        }    
+        else{
+            return isSubsequence(s,t,i,j+1);
+        }
+    }
+}
+*/
