ca4180c4647a42cc53512ade9432ac46ca067638
PranavFokmare-Dev <80598054+PranavFokmare-Dev@users.noreply.github.com>
2022-09-02 16:28:51 +0530

    array dp

--- 746. Min Cost Climbing Stairs.java
+++ 746. Min Cost Climbing Stairs.java
@@ -1,14 +1,10 @@
 class Solution {
         HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
     public int minCostClimbingStairs(int[] cost) {
-        return Math.min(f(0,cost),f(1,cost));
-    }
-    private int f(int index, int[] cost){
-        if(index>=cost.length)return 0;
-        if(map.containsKey(index))return map.get(index);
-        
-        int ans = Math.min(f(index+1,cost),f(index+2,cost))+cost[index];
-        map.put(index,ans);
-        return map.get(index);
+        int[] minCost = new int[cost.length+1];
+        for(int i = 2; i<minCost.length;i++){
+            minCost[i] = Math.min(minCost[i-1]+cost[i-1],minCost[i-2]+cost[i-2]);
+        }
+        return minCost[minCost.length - 1];
     }
 }
