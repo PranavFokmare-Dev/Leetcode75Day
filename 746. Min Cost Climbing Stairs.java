9c9e4ab394361bb86679ba9f57ac0257defc69bf
PranavFokmare-Dev <80598054+PranavFokmare-Dev@users.noreply.github.com>
2022-09-02 16:29:15 +0530

    O(1) in dp

--- 746. Min Cost Climbing Stairs.java
+++ 746. Min Cost Climbing Stairs.java
@@ -1,10 +1,13 @@
 class Solution {
         HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
     public int minCostClimbingStairs(int[] cost) {
-        int[] minCost = new int[cost.length+1];
-        for(int i = 2; i<minCost.length;i++){
-            minCost[i] = Math.min(minCost[i-1]+cost[i-1],minCost[i-2]+cost[i-2]);
+        int x = 0;
+        int y = 0;
+        for(int i = 2; i<cost.length+1;i++){
+            int temp = Math.min(y+cost[i-1],x+cost[i-2]);
+            x = y;
+            y = temp; 
         }
-        return minCost[minCost.length - 1];
+        return y;
     }
 }
