d750f94733cab1acef2960eba85136700e519617
PranavFokmare-Dev <80598054+PranavFokmare-Dev@users.noreply.github.com>
2022-09-05 19:06:13 +0530

    dp O(n2)

--- 62. Unique Paths.java
+++ 62. Unique Paths.java
@@ -1,15 +1,17 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] mem = new int[m][n];
-        return uniquePathsRec(m-1,n-1,mem);
-    }
-    private int uniquePathsRec(int x, int y,int[][] mem){
-        if(x ==0 || y ==0)
-            return 1;
-        if(x< 0 || y <0)
-            return 0;
-        if(mem[x][y]!=0)return mem[x][y];
-        mem[x][y] = uniquePathsRec(x,y-1,mem) + uniquePathsRec(x-1,y,mem);
-        return mem[x][y];
+        for(int r = 0; r<m;r++){
+            mem[r][0] = 1;
+        }
+        for(int c = 0;c<n;c++){
+            mem[0][c] = 1;
+        }
+        for(int i=1; i< m;i++){
+            for(int j =1;j<n;j++ ){
+                mem[i][j] = mem[i-1][j]+mem[i][j-1];
+            }
+        }
+        return mem[m-1][n-1];
     }
 }
