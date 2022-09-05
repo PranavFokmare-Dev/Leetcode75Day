309bab48ca27bd86522a9b44d27d72cbf090d640
PranavFokmare-Dev <80598054+PranavFokmare-Dev@users.noreply.github.com>
2022-09-05 20:05:24 +0530

    Create 1. Two Sum.java

---
+++ 1. Two Sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[]{-1,-1};
+        HashMap<Integer,Integer> indexOf = new HashMap<Integer,Integer>();
+        for(int i = 0; i<nums.length;i++){
+            if(indexOf.containsKey(target-nums[i]))
+            {
+                return new int[]{indexOf.get(target-nums[i]),i};
+            }
+            indexOf.put(nums[i],i);
+        }
+        return ans;
+    }
+}
