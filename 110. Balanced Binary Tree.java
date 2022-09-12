248f329aad971de5dc6a8006885da89fce13fe2f
PranavFokmare-Dev <80598054+PranavFokmare-Dev@users.noreply.github.com>
2022-09-12 12:26:18 +0530

    Update 110. Balanced Binary Tree.java

--- 110. Balanced Binary Tree.java
+++ 110. Balanced Binary Tree.java
@@ -14,22 +14,38 @@
  * }
  */
 class Solution {
-    HashMap<TreeNode,Integer> heightOf = new HashMap<TreeNode,Integer>();
+    //HashMap<TreeNode,Integer> heightOf = new HashMap<TreeNode,Integer>();
     private int hieght(TreeNode root){
         if(root == null)
             return 0;
-        if(heightOf.containsKey(root))return heightOf.get(root);
+       // if(heightOf.containsKey(root))return heightOf.get(root);
         int ans = 1 + Math.max(hieght(root.right),hieght(root.left));
-        heightOf.put(root,ans);
+       // heightOf.put(root,ans);
         return ans;
     }
     public boolean isBalanced(TreeNode root) {
         
         if(root == null)return true;
         
-        int left = hieght(root.left);
-        int right = hieght(root.right);
-        if(Math.abs(right-left)>1)return false;
-        return isBalanced(root.left) && isBalanced(root.right);
+        Pair result = getHeightDiff(root);
+        return result.ans;
+    }
+    
+    public class Pair{
+        int height;
+        boolean ans;
+        public Pair(boolean a,int h){
+            this.height = h;
+            this.ans = a;
+        }
+    }
+    
+    private Pair getHeightDiff(TreeNode root){
+        if(root == null)return new Pair(true,0);
+        Pair left = getHeightDiff(root.left);
+        if(!left.ans)return new Pair(false,-1);
+        Pair right = getHeightDiff(root.right);
+        if(!right.ans)return new Pair(false,-1);
+        return new Pair(Math.abs(right.height-left.height)<=1,1+Math.max(right.height,left.height));
     }
 }
