460bcee41ca3821e9116a66d579c8394864ab4dc
PranavFokmare-Dev <80598054+PranavFokmare-Dev@users.noreply.github.com>
2022-09-13 15:17:45 +0530

    Time: 8 ms (58.26%), Space: 44.6 MB (44.91%) - LeetHub

---
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -0,0 +1,22 @@
+class Solution {
+    int path = 0;
+    public void fn(TreeNode root, long sum, int target,HashMap<Long,Integer> hm){
+        if(root == null) return;
+        
+        sum += root.val;
+        if(hm.containsKey(sum-target))
+            path += hm.get(sum-target);
+        hm.put(sum, hm.getOrDefault(sum,0)+1);
+        
+        fn(root.left, sum, target,hm);
+        fn(root.right, sum, target,hm);
+        
+        hm.put(sum,hm.get(sum)-1);
+    }
+    public int pathSum(TreeNode root, int targetSum) {
+        HashMap<Long,Integer> hm = new HashMap<>();
+        hm.put((long)0,1);
+        fn(root, 0, targetSum,hm);
+        return path;
+    }
+}
