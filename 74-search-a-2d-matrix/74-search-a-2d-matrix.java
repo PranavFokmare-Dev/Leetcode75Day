32fa696e9e484009362a7b2b1ef6019c4d5aceed
PranavFokmare-Dev <80598054+PranavFokmare-Dev@users.noreply.github.com>
2022-09-14 06:54:58 +0530

    Time: 21 ms (5.04%), Space: 43.8 MB (5.37%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int index = findInFictional(matrix,target);
+        if(index==-1)return false;
+        return getElement(index,matrix) == target;
+    }
+    private int findInFictional(int[][] matrix, int target){
+        int start = 0;
+        int end = matrix.length*matrix[0].length-1;
+        while(start<=end){
+            int mid = end-((end-start)/2);
+            int curr = getElement(mid,matrix);
+            if(curr==target){
+                return mid;
+            }
+            else if(curr<target){
+                start = mid+1;
+            }
+            else{
+                end = mid -1;
+            }
+            
+        }
+        return -1;
+    }
+    private int getElement(int x, int[][] matrix){
+        int row = x/matrix[0].length;
+        int column = x%matrix[0].length;
+        System.out.println(x +" "+row+":"+column);
+        return matrix[row][column];
+    }
+}
